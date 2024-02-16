7de7d90767afa47049769447ce677d48955e06dd
Nish-mitha <nishmithasullal@gmail.com>
2024-02-16 11:27:38 +0530

    Initial Commit

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -7,9 +7,9 @@ export const SWAGGER_DESCRIPTION = "<h5>Google Drive Video Download & Upload API
                                     <h5>Video: <a href='https://drive.google.com/drive/folders/1MtYO1ORKVEzfABt3bVwyG1CyNwrkRTL9?usp=sharing'>Download folder link</a>, <a href='https://drive.google.com/drive/folders/1LgzxlGpkC5Fg-f-KL0bvucgbIVz8rkXk?usp=sharing'>Upload folder link</a></h5>\
                                     <h5>Destination Folder ID: 1LgzxlGpkC5Fg-f-KL0bvucgbIVz8rkXk</h5>\
                                     <h5>File IDs</h5>\
-                                    <h6>1. 1thGYHGVw71UPqNeQ4gvq54ARwPb7akB2 (10MB)</h6>\
+                                    <h6>1. 1thGYHGVw71UPqNeQ4gvq54ARwPb7akB2 (17KB)</h6>\
                                     <h6>2. 1Av4RE9WXkpNCI49m8sGV9iUPwlWvKShY (1MB)</h6>\
                                     <h6>3. 1eZal_hx07sBIouOCy8kVC9hrNgWwh9a_ (30MB)</h6>\
-                                    <h6>4. 1FzEgMBnHJtuCOcj33cwoPFf1sB1dx1Ck (100MB)</h6>\
+                                    <h6>4. 1FzEgMBnHJtuCOcj33cwoPFf1sB1dx1Ck (0bytes)</h6>\
                                     <h6>5. 1zCrOmXPJ-0qm7q-N3FI15ZHt5duhFuEp (500MB)</h6>\
                                     "
